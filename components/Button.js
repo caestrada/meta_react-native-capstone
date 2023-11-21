04347ce1dad088729d4c8502644e09838eefbc46
Carlos Estrada <cestr008@gmail.com>
2023-11-21 14:27:04 -0500

    add Avater component

--- components/Button.js
+++ components/Button.js
@@ -1,14 +1,20 @@
 import * as React from "react";
 import { Pressable, Text, StyleSheet } from "react-native";
 
-const Button = ({onPress, children, disabled}) => {
+const Button = ({ onPress, children, disabled, variant }) => {
   return (
     <Pressable
       onPress={onPress}
-      style={[styles.buttonWrapper, disabled && styles.disabled]}
+      style={[
+        styles.buttonWrapper,
+        disabled && styles.disabled,
+        variant === "outline" ? styles.outline : null,
+      ]}
       disabled={disabled}
     >
-      <Text style={styles.text}>{children}</Text>
+      <Text style={[styles.text, variant === "outline" ? styles.outlineText : null]}>
+        {children}
+      </Text>
     </Pressable>
   );
 };
@@ -16,19 +22,27 @@ const Button = ({onPress, children, disabled}) => {
 const styles = StyleSheet.create({
   buttonWrapper: {
     borderRadius: 8,
-    backgroundColor: '#495E57',
-    flexDirection: 'row',
-    justifyContent: 'center',
+    backgroundColor: "#495E57",
+    flexDirection: "row",
+    justifyContent: "center",
     padding: 8,
   },
+  outline: {
+    backgroundColor: "transparent",
+    borderWidth: 1,
+    borderColor: "#495E57",
+  },
   disabled: {
-    backgroundColor: 'grey',
+    backgroundColor: "grey",
     opacity: 0.5,
   },
   text: {
     fontSize: 16,
-    color: 'white',
-  }
+    color: "white",
+  },
+  outlineText: {
+    color: "#495E57",
+  },
 });
 
 export default Button;
--- screens/Profile.js
+++ screens/Profile.js
@@ -1,22 +1,18 @@
-import {
-  View,
-  Text,
-  Image,
-  TextInput,
-  Button,
-  TouchableOpacity,
-  ScrollView,
-  StyleSheet,
-  CheckBox,
-} from "react-native";
+import { View, Text, TextInput, ScrollView, StyleSheet } from "react-native";
 import React, { useState, useEffect } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { MaskedTextInput } from "react-native-mask-text";
+import * as ImagePicker from "expo-image-picker";
+import Avatar from "../components/Avatar";
+import Button from "../components/Button";
 
 export default function Profile() {
   const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
+  const [imageUri, setImageUri] = useState(null);
   const [orderStatuses, setOrderStatuses] = useState(false);
   const [passwordChanges, setPasswordChanges] = useState(false);
   const [specialOffers, setSpecialOffers] = useState(false);
@@ -39,25 +35,43 @@ export default function Profile() {
     loadProfile();
   }, []);
 
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+    });
+
+    if (!result.cancelled) {
+      setImageUri(result.uri);
+    }
+  };
+
   return (
     <ScrollView style={styles.container}>
       <Text style={styles.title}>Personal information</Text>
-      <Text style={styles.subheading}>Avatar</Text>
 
       <View style={styles.avatarSection}>
-        {/* Add avatar image here */}
-        <TouchableOpacity style={styles.button}>
-          <Text>Change</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>
-          <Text>Remove</Text>
-        </TouchableOpacity>
+        <Avatar imageUri={imageUri} firstName={firstName} lastName={lastName} />
+        <View style={styles.avatarSectionButtons}>
+          <Button onPress={pickImage}>Change</Button>
+          <Button variant="outline" onPress={() => setImageUri(null)}>
+            Remove
+          </Button>
+        </View>
       </View>
       <View style={styles.form}>
         <TextInput
           placeholder="First name"
           value={firstName}
           onChangeText={setFirstName}
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Last name"
+          value={lastName}
+          onChangeText={setLastName}
           style={styles.input}
         />
         <TextInput placeholder="Email" value={email} onChangeText={setEmail} style={styles.input} />
@@ -73,9 +87,9 @@ export default function Profile() {
         {/* Add CheckBox components and labels here */}
       </View>
       <View style={styles.footerButtons}>
-        <Button title="Log out" onPress={() => {}} />
-        <Button title="Save changes" onPress={() => {}} />
-        <Button title="Discard changes" onPress={() => {}} />
+        <Button onPress={() => {}}>Log out</Button>
+        <Button onPress={() => {}}>Save changes</Button>
+        <Button onPress={() => {}}>Discard changes</Button>
       </View>
     </ScrollView>
   );
@@ -93,11 +107,18 @@ const styles = StyleSheet.create({
     fontSize: 22,
     fontWeight: "bold",
   },
-  subheading: {
-    fontSize: 12,
-    marginBottom: 5,
-    color: "#777",
+  avatarSection: {
+    marginVertical: 20,
+    flexDirection: "row",
+    alignItems: "center",
   },
+  avatarSectionButtons: {
+    marginTop: 20,
+    marginLeft: 20,
+    flexDirection: "row",
+    gap: 20,
+  },
+
   input: {
     // height: 40,
     // margin: 12,
