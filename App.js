dddd12c4430e132667315d8443448f16c5ba46a8
Carlos Estrada <cestr008@gmail.com>
2023-11-21 07:49:46 -0500

    setup navigation

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
 import { NavigationContainer } from '@react-navigation/native';
 import Onboarding from './screens/Onboarding';
+import Profile from './screens/Profile';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useState } from 'react';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [isOnboardingCompleted, setIsOnboardingComplete] = useState(false);
+
+  // if (state.isLoading) {
+  //   return <SplashScreen />;
+  // }
+
   return (
     <NavigationContainer>
-      <Onboarding />
+      <Stack.Navigator>
+      {isOnboardingCompleted ? (
+        <Stack.Screen name="Profile" component={Profile} />
+      ) : (
+        <Stack.Screen name="Onboarding" component={Onboarding} />
+      )}
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
